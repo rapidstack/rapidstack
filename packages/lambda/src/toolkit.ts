3833555cd696d04fb71a9de5adae3d08bcbc7617
Zack Heil <57419337+zackheil@users.noreply.github.com>
2023-10-07 23:12:05 -0500

    toolkit factory idea

--- packages/lambda/src/toolkit.ts
+++ packages/lambda/src/toolkit.ts
@@ -1,14 +1,36 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// dummy brand types used as placeholders
+type Logger = {} & { _type: 'logger' };
+type Cache = {} & { _type: 'cache' };
+
+type Factory = <T>(tool: Creatable<T>) => T;
+
+type Creatable<T> = (
+  logger: Logger,
+  cache: Cache,
+  create: Factory,
+  options?: any
+) => T;
 /**
- *
- * @param name
+ * Creates a new lambda toolkit
+ * @param name the name of the application that the toolkit will bind to.
+ * @returns a toolkit
  */
-export function createToolkit(name: string) {
-  const createHandler = function createHandler() {
-    /* impl */
+export function createToolkit(name: string): {
+  create: Factory;
+  name: string;
+} {
+  const logger = {} as Logger;
+  const cache = {} as Cache;
+  // eslint-disable-next-line func-style
+  const create: Factory = function create<T>(tool: Creatable<T>): T {
+    return tool(logger, cache, create);
   };
 
   return {
-    createHandler,
+    create,
     name,
   };
 }

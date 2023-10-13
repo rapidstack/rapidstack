03b1414b48be52c22659670db4d9e36a2121dc23
Zack Heil <57419337+zackheil@users.noreply.github.com>
2023-10-12 22:09:31 -0500

    Factory idea 1

--- packages/lambda/src/toolkit.ts
+++ packages/lambda/src/toolkit.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-params */
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
@@ -5,14 +6,24 @@
 type Logger = {} & { _type: 'logger' };
 type Cache = {} & { _type: 'cache' };
 
-type Factory = <T>(tool: Creatable<T>) => T;
+type Factory = <C extends Creatable<T, O>, T, O extends CreatableOptions>(
+  tool: C,
+  options?: O
+) => T;
 
-type Creatable<T> = (
+type CreatableOptions<T extends Record<string, any> = {}> =
+  | ({
+      cacheTtl?: number;
+    } & { [K in keyof T]: T[K] })
+  | undefined;
+
+type Creatable<T, O extends CreatableOptions> = (
   logger: Logger,
   cache: Cache,
   create: Factory,
-  options?: any
+  options?: O
 ) => T;
+
 /**
  * Creates a new lambda toolkit
  * @param name the name of the application that the toolkit will bind to.
@@ -25,8 +36,12 @@ export function createToolkit(name: string): {
   const logger = {} as Logger;
   const cache = {} as Cache;
   // eslint-disable-next-line func-style
-  const create: Factory = function create<T>(tool: Creatable<T>): T {
-    return tool(logger, cache, create);
+  const create: Factory = function create<
+    C extends Creatable<T, O>,
+    T,
+    O extends CreatableOptions,
+  >(tool: C, options?: O): T {
+    return tool(logger, cache, create, options);
   };
 
   return {
@@ -34,3 +49,37 @@ export function createToolkit(name: string): {
     name,
   };
 }
+
+// setup for example:
+
+type IMyWidget = {
+  getItem: (key: string) => Promise<any>;
+  putItem: (key: string, value: any) => Promise<void>;
+};
+
+type IMyWidgetOptions = CreatableOptions<{
+  someProperty: string;
+}>;
+
+// eslint-disable-next-line func-style
+const MyWidget: Creatable<IMyWidget, IMyWidgetOptions> = (
+  logger,
+  cache,
+  create,
+  options
+): IMyWidget => {
+  const dynamo = {} as IMyWidget;
+  return dynamo;
+};
+
+// Example usage idea:
+
+const toolkit = createToolkit('my-app');
+const d = toolkit.create(MyWidget, { someProperty: 'my-property' });
+
+/* 
+The problem with this approach is that there is no intellisense on the options.
+Need to see if there is a better way to handle the creatable options so they
+fit handlers and other utils the lambda package exports so all things are 
+constructed in a consistent way.
+*/

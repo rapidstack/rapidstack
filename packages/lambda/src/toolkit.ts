62bc85b8e57dc0001f6e2a9465dc0ef9a37856e4
Zack Heil <57419337+zackheil@users.noreply.github.com>
2023-10-15 00:03:43 -0500

    Some progress on the toolkit's inner factory 2 implementation

--- packages/lambda/src/toolkit.ts
+++ packages/lambda/src/toolkit.ts
@@ -1,46 +1,80 @@
-/* eslint-disable max-params */
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 // dummy brand types used as placeholders
 type Logger = {} & { _type: 'logger' };
 type Cache = {} & { _type: 'cache' };
+declare type OptionsIndex = 3;
 
-type Factory = <C extends Creatable<T, O>, T, O extends CreatableOptions>(
+type IsConstructable<T> = T extends new (...args: any[]) => any ? 'yes' : 'no';
+
+// ---------------------------
+// Relevant TypeScript Implementation
+// ---------------------------
+
+type Factory = <
+  C extends Creatable<T, O>,
+  T extends ICreatable,
+  O extends ICreatableOptions,
+>(
   tool: C,
-  options?: Parameters<C>[3]
+  options?: C extends new (...args: any[]) => any
+    ? ConstructorParameters<C>[OptionsIndex]
+    : C extends (
+        logger: Logger,
+        cache: Cache,
+        create: Factory,
+        options?: O
+      ) => T
+    ? Parameters<C>[OptionsIndex]
+    : never
 ) => T;
 
-type StandardCreateOptions = {
+interface ICreatable {}
+interface ICreatableOptions {
   cacheTtl?: number;
-};
+}
 
-type CreatableOptions<T extends Record<string, any> = Record<string, any>> = {
-  [K in keyof T]: T[K];
-} & StandardCreateOptions;
-
-type Creatable<T, O extends CreatableOptions> = (
-  logger: Logger,
-  cache: Cache,
-  create: Factory,
-  options?: O
-) => T;
+type Creatable<T, O extends ICreatableOptions> =
+  | ((logger: Logger, cache: Cache, create: Factory, options?: O) => T)
+  | {
+      new (logger: Logger, cache: Cache, create: Factory, options?: O): T;
+    };
 
 /**
- * Creates a new lambda toolkit
- * @param name the name of the application that the toolkit will bind to.
- * @returns a toolkit
+ *
+ * @param name
  */
-export function createToolkit(name: string): {
-  create: Factory;
-  name: string;
-} {
+export function createToolkit(name: string) {
   const logger = {} as Logger;
   const cache = {} as Cache;
-  // eslint-disable-next-line func-style
-  const create: Factory = function create(tool, options?) {
-    return tool(logger, cache, create, options);
-  };
+
+  function create<T extends ICreatable>(
+    tool: Creatable<T, ICreatableOptions>
+  ): T;
+  function create<T extends ICreatable>(
+    tool: Creatable<T, ICreatableOptions>
+  ): T;
+  function create<T extends ICreatable, O extends ICreatableOptions>(
+    tool: Creatable<T, O>,
+    options: O
+  ): T;
+  /**
+   *
+   * @param tool
+   * @param options
+   */
+  function create<T extends ICreatable, O extends ICreatableOptions>(
+    tool: Creatable<T, O>,
+    options?: O
+  ): T {
+    if (isConstructible(tool)) {
+      return new (tool as {
+        new (logger: Logger, cache: Cache, create: Factory, options?: O): T;
+      })(logger, cache, create, options);
+    }
+
+    return (
+      tool as (logger: Logger, cache: Cache, create: Factory, options?: O) => T
+    )(logger, cache, create, options);
+  }
 
   return {
     create,
@@ -48,35 +82,104 @@ export function createToolkit(name: string): {
   };
 }
 
-// setup for example:
+// ---------------------------
+// Example Setup Code
+// ---------------------------
 
-type IMyWidget = {
+interface IMyWidget extends ICreatable {
   getItem: (key: string) => Promise<any>;
   putItem: (key: string, value: any) => Promise<void>;
-};
-
-interface IMyWidgetOptions extends CreatableOptions {
-  someProperty?: string;
 }
 
-// eslint-disable-next-line func-style
-const MyWidget: Creatable<IMyWidget, IMyWidgetOptions> = (
-  logger,
-  cache,
-  create,
-  options
-): IMyWidget => {
-  const something = {} as IMyWidget;
-  return something;
+interface IMyOptionalWidgetOptions extends ICreatableOptions {
+  someProperty?: string;
+}
+interface IMyRequiredWidgetOptions extends ICreatableOptions {
+  myProp?: string;
+  someProperty: string;
+}
+
+const MyWidgetWithOptionalOptions: Creatable<
+  IMyWidget,
+  IMyOptionalWidgetOptions
+> = (logger, cache, create, options): IMyWidget => {
+  const widget = {} as IMyWidget; // dummy impl
+  return widget;
 };
 
-// Example usage idea:
+const MyWidgetWithRequiredOptions: Creatable<
+  IMyWidget,
+  IMyRequiredWidgetOptions
+> = (logger, cache, create, options): IMyWidget => {
+  const widget = {} as IMyWidget; // dummy impl
+  return widget;
+};
+
+// --- for not attempted class version yet:
+
+// interface IClassCreatable {
+//   new: (logger: Logger, cache: Cache, create: Factory, options: IMyClassWidgetOptions)
+// }
+
+interface IMyClassWidget extends ICreatable {
+  deleteSomething: (key: string) => Promise<any>;
+  setSomething: (key: string, value: any) => Promise<any>;
+}
+
+interface IMyClassWidgetOptions extends ICreatableOptions {
+  someOtherProperty: string;
+}
+
+class MyClassWidget
+  implements Creatable<IMyClassWidget, IMyClassWidgetOptions>
+{
+  constructor(
+    logger: Logger,
+    cache: Cache,
+    create: Factory,
+    options: IMyClassWidgetOptions
+  ) {}
+
+  public async deleteSomething(key: string) {}
+  public async setSomething(key: string, value: any) {}
+}
+
+// ---------------------------
+// Test Cases
+// ---------------------------
 
 const toolkit = createToolkit('my-app');
-const d = toolkit.create(MyWidget);
+const MyWidget = MyWidgetWithOptionalOptions;
+const test1 = toolkit.create(MyWidget, { someProperty: 'my-property' }); // should return type IMyWidget
+//    ^?
 
-/*
-This looks to be closer to what is wanted if it can be assumed that options will
-always be the 4th parameter (line 11). The problem here is that the return type
-from the factory is unknown. Need to fix that...
+// 1. the output of factory must reflect the interface of the MyWidget: PASS
+
+// -------------------------------------------------------------------------------------------
+
+const test2 = toolkit.create(MyWidgetWithOptionalOptions, {}); // should not error
+const test21 = toolkit.create(MyWidgetWithRequiredOptions); // should error
+
+// 2. the factory must be conscious of the properties (if required) to create: PASS
+
+// -------------------------------------------------------------------------------------------
+
+const test3 = toolkit.create(MyWidgetWithOptionalOptions); // should not error
+const test31 = toolkit.create(MyWidgetWithRequiredOptions); // should error
+
+// 3. the factory must complain if there is a required property for the second parameter and it isn't supplied: PASS
+
+// -------------------------------------------------------------------------------------------
+
+type t = IsConstructable<typeof MyWidget>;
+//   ^?
+
+const test4 = toolkit.create(MyClassWidget);
+
+// 4. support classes and factory function/closures that return methods/properties: FAIL (have not tried yet; not implemented)
+
+// -------------------------------------------------------------------------------------------
+
+/* 
+Now 1, 2, and 3 are working. Just need to get this working with classes.
 */

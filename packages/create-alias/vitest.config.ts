ad107a79ee0c368746e823bd2e3b7e4ba7647e14
Zack Heil <57419337+zackheil@users.noreply.github.com>
2023-10-31 22:39:26 -0500

    Fix failing tests

--- packages/create-alias/vitest.config.ts
+++ packages/create-alias/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
-    name: '@rapidstack/create',
+    name: '@rapidstack/create (alias)',
     passWithNoTests: true,
   },
 });
